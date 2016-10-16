3e0981a35bca17376f3229b1ed0a64b9a68e5106
Andrea Cardy <acardy@customink.com>
2016-10-15 22:51:19 -0400

    refactor and repair of your text analyzer project to discuss during 10/16 session

--- app.js
+++ app.js
@@ -1,103 +1,82 @@
-function totalWordCount (string) {
-	var split = string.split(" ");
-	var count = 0; 
-	var letters = /[a-z]/;
-	console.log(split);
-	split.forEach(function(word) {
-		if (word.match(letters)) {
-			count++; 
-		}	
-	});
-	
-	return count; 
-}
-
-function uniqueWordCount(string) {
-	var withoutPunctuation = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-	var split = withoutPunctuation.split(" "); 
-	var database = {}; 
-	var uniqueCount = 0; 
-	
-	split.forEach(function(word) {
-		database[word] === undefined ? database[word] = 1 : database[word]++; 
-	}); 
-	
-	for (var key in database) {
-		if (database[key] === 1) {
-			uniqueCount++; 
-		}
-	}
-	
-	return uniqueCount;
-}
-
-
-function averageWordLength(string) {
-	var split = string.split(" ");
-	var lengthArray = []; 
-	var letters = /[a-z]/;
-	
-	split.forEach(function (word) {
-		word.replace(/\./g,' ');
-		if (word.match(letters)) {
-			lengthArray.push(word.length);
-		}
-	}); 
-	
-	var sum = lengthArray.reduce(function(acc, current) {
-		return acc + current; 
-	}); 
-	
-	var average = sum/lengthArray.length; 
-	
-	return Number(average.toFixed(2)); 
-}
-
-function averageSentenceLength (string) {
-	var split = string.split(".");
-	var sentenceLengths = []; 
-	var temp; 
-	var letters = /[a-z]/;
-	var count; 
-	
-	split.forEach(function(sentence) {
-		count = 0; 
-		temp = sentence.split(" ");
-		temp.forEach(function(words) {
-			if (words.match(letters)) {
-				count++; 
-			}
-		});
-		sentenceLengths.push(count);
-	});
-
-	
-	var sum = sentenceLengths.reduce(function(acc, current) {
-		return acc + current; 
-	});
-	
-	var average = sum/sentenceLengths.length; 
-	
-	return Number(average.toFixed(2)); 
-
-}
-
-
-
 $(function() {
-  	
+    
  $('#user-input').submit(function(e) {
     e.preventDefault();
     var userInput = $('#user-text').val();
+    var arrayOfWords = getArrayofWords(userInput);
    $("dl").removeClass("hidden");
-   console.log(totalWordCount(userInput));
-   $(".js-word-count").append('</br>' + totalWordCount(userInput));
-   $(".js-unique-word").append('</br>' + uniqueWordCount(userInput));
-   $(".js-average-word").append('</br>' + averageWordLength(userInput));
-   console.log(averageSentenceLength(userInput));
-   $(".js-average-sentence").append('</br>' + averageSentenceLength(userInput));
-
-   
+   $(".js-word-count").append('</br>' + totalWordCount(arrayOfWords));
+   $(".js-unique-word").append('</br>' + uniqueWordCount(arrayOfWords));
+   $(".js-average-word").append('</br>' + averageWordLength(arrayOfWords));
+   $(".js-average-sentence").append('</br>' + averageSentenceLength(userInput,arrayOfWords));
   });
 
  });
+
+function getArrayofWords(string) {
+  var split = string.match(/\b[^\s]+\b/g);
+  return split;
+}
+
+function totalWordCount (wordArray) {
+  
+  /**
+   * Your original var 'split'...
+   * var split = string.split(" ");
+   * for "Hi I am Andrea. I am a person."
+   * returns ["Hi", "I", "am", "Andrea.", "I", "am", "a", "person."]
+   */ 
+
+  var count = 0;
+  // console.log(split);
+  /**
+   * for "Hi I am Andrea. I am a person."
+   * console.log(split)
+   * returns ["Hi", "I", "am", "Andrea", "I", "am", "a", "person"]
+   */
+
+  wordArray.forEach(function(word) {
+      count++; 
+  });
+  
+  return count; 
+}
+
+function uniqueWordCount(wordArray) {
+  var database = {};
+  var uniqueCount;
+  
+  wordArray.forEach(function(word) {
+    database[word] === undefined ? database[word] = 1 : database[word]++; 
+  });
+  /**
+   * 
+   * for "Hi I am Andrea. I am a person."
+   * console.log(database);
+   * returns {Andrea:1, Hi:1, I:2, a:1, am:2, person:1}
+   */
+  
+  uniqueCount = Object.keys(database).length;
+  return uniqueCount;
+}
+
+
+function averageWordLength(wordArray) {
+  var lengthArray = [];
+  var sum = 0;
+  var average;
+  
+  wordArray.forEach(function (word) {
+    sum = sum + word.length;
+  });
+  
+  average = sum/wordArray.length;
+  return Number(average.toFixed(2)); 
+}
+
+function averageSentenceLength (string, wordArray) {
+  var numSentences = string.match(/[.!?]+/g).length;
+  var wordCount = wordArray.length;
+  
+  return (wordCount / numSentences).toFixed(2);
+}
